9161e311cda4b65629176854114177b333db8ff1
민동현 <mindonghyeon890@gmail.com>
2019-12-21 03:23:22 +0900

    커밋

---
+++ src/main/java/com/donghyeon/effectivejava/rule19/PhysicalConstants.java
@@ -0,0 +1,11 @@
+package com.donghyeon.effectivejava.rule19;
+
+//상수 인터페이스 안티패턴 - 사용하지 말것!
+public interface PhysicalConstants {
+    //아보가드로 수(1/mol)
+    static final double AVOGADROS_NUMBER = 6.02214199e23;
+
+    //전자 질량(kg)
+    static final double ELECTRON_MASS = 9.10938188e-31;
+}
+
---
+++ src/main/java/com/donghyeon/effectivejava/rule19/Rule19.java
@@ -0,0 +1,8 @@
+package com.donghyeon.effectivejava.rule19;
+
+public class Rule19 {
+
+ public static void main(String[] args) {
+
+  }
+}
